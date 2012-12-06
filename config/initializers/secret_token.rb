566979541441ac92ab5bf0aa22dd401d45c4d563
MontyLov <montylov@gmail.com>
2012-12-06 16:45:32 -0500

    pure rack app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '683e84e253cd4aa469c6f62acd81f5b78b3f1c3c89348059164bbd7532a98d4dc2b2a0065e0bc5edebbc530280c1c729f87ed7e380a637cfa541bace1f61e860'
